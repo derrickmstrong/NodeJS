5eadf729db52d241f95f1299b66a227e5401b3d5
Derrick Strong <derrickmstrong@gmail.com>
2020-06-23 20:18:14 -0500

    Clean copy

--- server/server.js
+++ server/server.js
@@ -3,23 +3,7 @@ const fs = require('fs');
 
 let chirpsPath = path.join(__dirname, '../chirps.json');
 
-// Part 1 - Console.log chirps.json
-fs.readFile(
-  chirpsPath,
-  {
-    encoding: 'utf-8',
-  },
-  (err, data) => {
-    console.log(data);
-
-    const chirpsData = JSON.parse(data);
-    chirpsData.forEach((item) => {
-      console.log(item.username);
-    });
-  }
-);
-
-// v2 Write json array to file
+// Part 1 - Write json array to file
 const chirps = [
   {
     username: 'Derrick M',
@@ -44,3 +28,19 @@ const chirps = [
 ];
 const chirpsData = JSON.stringify(chirps);
 fs.writeFileSync('./chirps.json', chirpsData, () => console.log('done'));
+
+// Part 2 - Console.log chirps.json
+fs.readFile(
+  chirpsPath,
+  {
+    encoding: 'utf-8',
+  },
+  (err, data) => {
+    console.log(data);
+
+    const chirpsData = JSON.parse(data);
+    chirpsData.forEach((item) => {
+      console.log(item.username);
+    });
+  }
+);
